5749f8015a0251b11a297ca16060d8c78bd3b1e4
Amanda Oh <[DAmandaGoh>
2016-10-15 23:55:56 +0800

    created static folder to test routing

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ var express = require('express')
 var app = express()
 var port = 3000
 
+//set up routes
+var static_routes = require('./routes/index')
+
+//set middleware for routes
+app.use('/static' , static_routes)
+
+//the view engine for express is ejs. Hence, res.render(index) => index.ejs inside VIEWS folder
+app.set('view engine', 'ejs')
+
+//capture all the request, assuming all static files is inside public foler
+app.use(express.static(__dirname + '/static'))
+
+app.get('/', function (req, res){
+  res.render('index', {header: 'Bomb Sweeper'})
+})
+
+
 app.listen(port)
-
-//The static middleware handles serving up the content from a directory. In this case the 'root' directory is served up and any content (HTML, CSS, JavaScript) will be available.
-app.use(express.static(__dirname + '/'))
-
 console.log('Server running at http://localhost: ' + port + '/')
